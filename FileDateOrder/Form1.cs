af550659067b4d46837fcc84590428ea82af5c51
griszmen <griszsz@gmail.com>
2015-02-27 20:44:41 +0200

    basic functionality added

--- FileDateOrder/Form1.cs
+++ FileDateOrder/Form1.cs
@@ -16,17 +16,19 @@ namespace FileDateOrder
         public Form1()
         {
             InitializeComponent();
-        }
+            richTextBox1.Text = "";                  
+       }
 
         string path;
 
         private void button1_Click(object sender, EventArgs e)
         {
+            richTextBox1.Text = "";
             FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
 
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
-                path = folderBrowserDialog1.SelectedPath;
+                path = folderBrowserDialog1.SelectedPath;              
             }
 
             textBox1.Text = path;
@@ -35,8 +37,34 @@ namespace FileDateOrder
 
             foreach (string file in files)
             {
+                
                 DateTime creationDate = File.GetLastWriteTime(file);
-            }            
+                int fileYear = creationDate.Year;
+                int fileMonth = creationDate.Month;
+                string newPath = path + "\\" + fileYear + "\\" + fileMonth;
+
+                if (!Directory.Exists(newPath))
+                
+                {
+                    Directory.CreateDirectory(newPath);
+                    //MessageBox.Show("Folder " + newPath + " created!");
+                }
+
+                string fileName = Path.GetFileName(file);
+                string newPathFull = Path.Combine(newPath,fileName);
+
+                if (!File.Exists(newPathFull))
+                {
+                    File.Copy(file, newPathFull);
+                    richTextBox1.Text += file + " Modification date: " + creationDate + " copied to: " + newPathFull + "\n";
+                }                   
+                
+            }
+
+            if (richTextBox1.Text == "")
+            {
+                MessageBox.Show("Everything is on its place!");
+            }
 
         }
     }
