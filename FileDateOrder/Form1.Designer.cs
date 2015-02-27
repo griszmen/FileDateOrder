8e8a5b72cd8f0d1981d955b866adfc471a4ac9c8
griszmen <griszsz@gmail.com>
2015-02-27 23:55:17 +0200

    almost finish

--- FileDateOrder/Form1.Designer.cs
+++ FileDateOrder/Form1.Designer.cs
@@ -31,43 +31,94 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 26);
+            this.button1.Location = new System.Drawing.Point(20, 42);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 20);
+            this.button1.Size = new System.Drawing.Size(93, 21);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Set Path";
+            this.button1.Text = "Set parent path";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(141, 27);
+            this.textBox1.Location = new System.Drawing.Point(141, 42);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(282, 20);
             this.textBox1.TabIndex = 1;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(141, 85);
+            this.richTextBox1.Location = new System.Drawing.Point(141, 82);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(788, 515);
+            this.richTextBox1.Size = new System.Drawing.Size(541, 423);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "*.jpg",
+            "*.mov",
+            "*.mp4",
+            "*.txt"});
+            this.comboBox1.Location = new System.Drawing.Point(20, 92);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(93, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Choose file format...";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Move Files";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(356, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "This program moves files of chosen type to folder parent_path/year/month";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(941, 612);
+            this.ClientSize = new System.Drawing.Size(694, 517);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FileDateOrder";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,6 +129,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- FileDateOrder/Form1.cs
+++ FileDateOrder/Form1.cs
@@ -16,56 +16,85 @@ namespace FileDateOrder
         public Form1()
         {
             InitializeComponent();
-            richTextBox1.Text = "";                  
-       }
+            richTextBox1.Text = "";
+            comboBox1.Text = "*.jpg";     
+        }
 
-        string path;
+        string path = "";
+        string[] monthNames = {"empty", "01-Jan", "02-Feb", "03-March", "04-April", "05-May", "06-June", "07-July", "08-Aug", "09-Sep", "10-Oct", "11-Nov", "12-Dec"};
+
+         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            richTextBox1.Text = "";
+
+            textBox1.Text = "";
+            richTextBox1.Text = "";            
             FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
 
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
-                path = folderBrowserDialog1.SelectedPath;              
-            }
-
-            textBox1.Text = path;
-
-            string[] files = Directory.GetFiles(path, "*.txt", SearchOption.AllDirectories);
-
-            foreach (string file in files)
-            {
-                
-                DateTime creationDate = File.GetLastWriteTime(file);
-                int fileYear = creationDate.Year;
-                int fileMonth = creationDate.Month;
-                string newPath = path + "\\" + fileYear + "\\" + fileMonth;
-
-                if (!Directory.Exists(newPath))
-                
-                {
-                    Directory.CreateDirectory(newPath);
-                    //MessageBox.Show("Folder " + newPath + " created!");
-                }
-
-                string fileName = Path.GetFileName(file);
-                string newPathFull = Path.Combine(newPath,fileName);
-
-                if (!File.Exists(newPathFull))
-                {
-                    File.Copy(file, newPathFull);
-                    richTextBox1.Text += file + " Modification date: " + creationDate + " copied to: " + newPathFull + "\n";
-                }                   
-                
-            }
-
-            if (richTextBox1.Text == "")
-            {
-                MessageBox.Show("Everything is on its place!");
+                path = folderBrowserDialog1.SelectedPath;
+                textBox1.Text = path;               
             }
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.Text = "";
+            if (path == "")
+            {
+                MessageBox.Show("Choose path first!");
+            }
+            else
+            {
+                string pattern = comboBox1.Text;
+                string[] files = Directory.GetFiles(path, pattern, SearchOption.AllDirectories);
+
+                //string numberOfFiles = files.Count().ToString();
+                DialogResult messageBoxResult = MessageBox.Show("Are you sure?", "Delete confirmation", MessageBoxButtons.YesNo);
+
+                if (messageBoxResult == DialogResult.Yes)
+                {
+                    foreach (string file in files)
+                    {
+
+                        DateTime creationDate = File.GetLastWriteTime(file);
+                        int fileYear = creationDate.Year;
+                        int fileMonth = creationDate.Month;
+                        string newPath = path + "\\" + fileYear + "\\" + monthNames[fileMonth];
+
+                        if (!Directory.Exists(newPath))
+                        {
+                            Directory.CreateDirectory(newPath);
+                            //MessageBox.Show("Folder " + newPath + " created!");
+                        }
+
+                        string fileName = Path.GetFileName(file);
+                        string newPathFull = Path.Combine(newPath, fileName);
+
+                        if (!File.Exists(newPathFull))
+                        {
+                            File.Move(file, newPathFull);
+                            richTextBox1.Text += file + " Modification date: " + creationDate + " copied to: " + newPathFull + "\n";
+                        }
+
+                    }
+
+                    if (richTextBox1.Text == "")
+                    {
+                        MessageBox.Show("Everything is on its place!");
+                    }
+                }
+
+            }
+            
+        }
+
     }
 }
